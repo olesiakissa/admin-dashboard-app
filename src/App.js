27ecc4f196f2908c35d547b7064c64f73bd392b7
Olesia Kissa <okissaw@gmail.com>
2023-06-27 15:07:35 +0200

    Add form component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Dashboard         from './scenes/dashboard/dashboard';
 import Team              from './scenes/team/Team';
 import Contacts          from './scenes/contacts/Contacts';
 import Invoices          from './scenes/invoices/Invoices';
+import Form              from './scenes/form/Form';
 // import Bar               from './scenes/bar';
-// import Form              from './scenes/form';
 // import Line              from './scenes/line';
 // import Pie               from './scenes/pie';
 // import FAQ               from './scenes/faq';
@@ -32,8 +32,8 @@ const App = () => {
               <Route path='/team'       element={ <Team /> } />
               <Route path='/contacts'   element={ <Contacts /> } />
               <Route path='/invoices'   element={ <Invoices /> } />
+              <Route path='/form'       element={ <Form /> } />
               {/* <Route path='/bar'        element={ <Bar /> } /> */}
-              {/* <Route path='/form'       element={ <Form /> } /> */}
               {/* <Route path='/line'       element={ <Line /> } /> */}
               {/* <Route path='/pie'        element={ <Pie /> } /> */}
               {/* <Route path='/faq'        element={ <FAQ /> } /> */}
